92484d0460558c16f066b5efca1263c2fc838419
mitcehub <mitcehub@users.noreply.github.com>
2026-05-27 15:23:13 +0800

    fix: remove background.scripts from Chrome manifest, add Firefox zip build script

---
+++ scripts/zip-firefox.mjs
@@ -0,0 +1,14 @@
+import AdmZip from 'adm-zip';
+import { readFileSync } from 'fs';
+
+const zip = new AdmZip();
+zip.addLocalFolder('Translate');
+zip.getEntries().filter(e => e.name.endsWith('.map')).forEach(e => zip.deleteFile(e));
+
+const mf = JSON.parse(zip.getEntry('manifest.json').getData().toString('utf-8'));
+delete mf.background.service_worker;
+mf.background = { scripts: ['static/js/background.js'] };
+mf.browser_specific_settings = { gecko: { id: 'ez-translate@mitcehub.github.io' } };
+zip.updateFile('manifest.json', Buffer.from(JSON.stringify(mf, null, 2), 'utf-8'));
+
+zip.writeZip('ez-translate-firefox.zip');
